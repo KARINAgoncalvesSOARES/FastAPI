535503b3a4c8f595890168e10ba8d412061b19f5
KARINAgoncalvesSOARES <karinagonsoares@gmail.com>
2024-04-11 16:43:17 -0300

    ETL  e criação do modelo.

--- 2_fastapi/1_fastapi_deployment_mercado.py
+++ 2_fastapi/1_fastapi_deployment_mercado.py
@@ -10,8 +10,8 @@ Link de estudo:
 """
 
 """
-Previsão de aplicativo de mercearia preferível na Hungria
-=========================================================
+Previsão de aplicativo de mercearia preferível na Hungria 🇭🇺
+============================================================
 
 Este aplicativo FastAPI foi projetado para prever o aplicativo de mercearia preferível na Hungria com base nas informações do usuário,
 como idade, sexo, nível de escolaridade, anos de experiência com compras on-line e anos de experiência no uso de aplicativos de mercearia.
@@ -43,3 +43,49 @@ Requer os seguintes parâmetros:
 
     * exp_app: Anos de experiência no uso de aplicativos de supermercado.
 """
+# Previsão simples do aplicativo de supermercado na Hungria (usando FastAPI)
+
+class_App_names = ['FoodPanda', 'Wolt', 'Spar', 'Tesco online', 'myLidl']
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+import pickle # Serialização e desserialização
+import os
+
+filePath = os.path.join(os.getcwd(), '2_fastapi/data_mercado.csv')
+
+# 1. Abrindo os dados para leitura
+data = pd.read_csv(filePath)
+
+# 2. Checando os dados
+#print (data.head(7))
+
+#3. Separando os dados em Features & Target
+x = data.drop("Apps", axis=1)
+y = data["Apps"]
+
+# 4. Separando os dados em treino e teste
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+
+# 5. Criando o modelo
+clf = RandomForestClassifier()
+
+# 6. Treinando o modelo
+clf.fit(x_train, y_train)
+
+# 7. Fanzendo a previsão
+y_preds = clf.predict(x_test)
+
+# 8. Calculando a acurácia
+acc = accuracy_score(y_test, y_preds)
+print(f"A acurácia do modelo é: {round((acc*100),2)}%")
+
+# 9. Salvando o modelo
+#filename = "clf_silple.pkl"
+#pickle.dump(clf, open(filename, 'wb'))
+
+# 10. carregue o modelo e faça uma previsão
+# # no aplicativo fastapi.py
+
